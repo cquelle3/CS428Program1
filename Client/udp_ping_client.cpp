982d31f388d474b1acd2e983dfc2603fd7b2c2cf
Colin Quelle <cquelle3@binghamton.edu>
2020-09-29 22:05:37 -0400

    Code runs on linux machine

--- Client/udp_ping_client.cpp
+++ Client/udp_ping_client.cpp
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/time.h>
 
 #define PORT 12000
 
@@ -16,15 +17,26 @@ int main(){
 
 	int msg_count = 0;
 
+	struct timeval t1;
+	struct timeval t2;
+	struct timeval res;
+
+	struct timeval tv;
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+
 	int sockfd;
-	int n;
 	socklen_t len;
 	char buffer[1024];
    	struct sockaddr_in servaddr;
 
-	const char *message = "Hello from client";
+	const char *message = "Hello!";
 
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if(setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (struct timeval *)&tv, sizeof(struct timeval)) < 0){
+		perror("Error");
+	}
 
 	memset(&servaddr, 0, sizeof(servaddr));
 
@@ -35,15 +47,24 @@ int main(){
 	while(msg_count < 10){
 
 		sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-		cout << "Message sent to server" << endl;
+		cout << "Message "<< msg_count+1  << " " << "sent to server." << endl;
 
-		n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-		buffer[n] = '\0';
-		cout << "Response received from server" << endl;
+		gettimeofday(&t1, NULL);
 
+		if(recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL, (struct sockaddr *) &servaddr, &len) < 0){
+			cout << "Packet was dropped." << endl;
+		}
+		else{
+
+			gettimeofday(&t2, NULL);
+			timersub(&t2, &t1, &res);
+
+			cout << "Server: " << buffer << " (RTT: " << res.tv_usec << " microseconds)" << endl;
+
+		}
 		msg_count++;
 	}
-	close(sockfd);
 
+	close(sockfd);
    	return 0;
 }
